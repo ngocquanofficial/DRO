fix bug forgot applying softmax

src/model.py
@@ -334,13 +334,14 @@ class ClassificationModel(pl.LightningModule):
                 pred_ = pred_ + pred[j]
             pred_ = pred_/max(1, self.num_particles)
             entropy_loss = self.loss_fn(pred_, y)
-            div_loss = - log_det(y, pred, self.num_particles).to(pred[0].device)
+            prob_pred = [torch.nn.functional.softmax(i, dim= -1) for i in pred]
+            div_loss = - log_det(y, prob_pred, self.num_particles).to(pred[0].device)
 
             loss = entropy_loss + div_loss
             
             # Get accuracy
             metrics = getattr(self, f"{mode}_metrics")(pred_, y.argmax(1))
-            avg_cosine, max_cosine, min_cosine = cal_cosine_similarity(y, pred, self.num_particles)
+            avg_cosine, max_cosine, min_cosine = cal_cosine_similarity(y, prob_pred, self.num_particles)
             self.log("DIV_LOSS", div_loss, prog_bar=True)
             self.log("max_cosine", max_cosine, prog_bar=True)
             self.log("min_cosine", min_cosine, prog_bar=True)

src/test.py
@@ -108,8 +108,10 @@ def cal_cosine_similarity(y_true, pred, num_models):
 y_true = torch.tensor([[0, 0, 0, 1], [1, 0, 0, 0]])
 pred1 = torch.tensor([[1.0,1.0,1.0,1.0], [4.0, 1.0, 0.0, 0.0]])
 pred2 = torch.tensor([[1.0,1.0,0.0,0.0], [4.0, 1.0, 0.0, 0.0]])
-pred3 = torch.tensor([[0.0,0.0,1.0,1.0], [4.0, 0.0, 0.0, 1.0]])
+pred3 = torch.tensor([[0.0,0.0,0.0,1.0], [4.0, 0.0, 0.0, 1.0]])
 
 pred_list = [pred1, pred2, pred3]
 
 print(cal_cosine_similarity(y_true, pred_list, 3))
+
+# print(torch.nn.functional.softmax(pred3, dim= -1))