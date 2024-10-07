5cf0e92a14aa73e3f85e9e81051d18bcc4a2d940
NgocQuan <ngocquanofficial@gmail.com>
2024-10-07 07:36:10 +0000

    nq-hard

--- src/model.py
+++ src/model.py
@@ -388,9 +388,9 @@ class ClassificationModel(pl.LightningModule):
             
 
             # Update lamda by hand 
-            print(raw_distance)
+
             lamda_ew = self.bound - final_distance.detach().clone()
-            self.lamda = torch.clamp(self.lamda - current_lr * lamda_ew, min= 1, max= 10)
+            self.lamda = torch.clamp(self.lamda - current_lr * lamda_ew, min= 1, max= 5)
 
 
             opt.zero_grad()
